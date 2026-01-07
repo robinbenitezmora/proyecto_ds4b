dcfb11721e5963ddf1fd21963f32d2334be6c7bf
Robin <robinbenitez2024@outlook.com>
2026-01-06 21:23:08 -0500

    Se corrige falla en codigo

--- scripts/app_abandono.py
+++ scripts/app_abandono.py
@@ -1,4 +1,10 @@
 # Script para analizar abandondo de clientes
 # Todo por desarrollar
 
-df['segmento_valor'] = pd.qcut(df['valor'], 3, labels=['bajo', 'medio', 'alto']) 
+import pandas as pd
+
+# Cargar datos de clientes
+df = pd.read_csv("data/clientes.csv")
+
+
+df["segmento_valor"] = pd.qcut(df["valor"], 3, labels=["bajo", "medio", "alto"])
